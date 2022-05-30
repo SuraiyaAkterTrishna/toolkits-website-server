076e4668b88fe31d0254de2287d7a811adcc1952
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-05-30 08:24:12 +0600

    Save registered user in database

--- index.js
+++ index.js
@@ -133,6 +133,23 @@ async function run() {
       const users = await cursor.toArray();
       res.send(users);
     });
+    // Save registered user in database
+    app.put("/user/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "771h" }
+      );
+      res.send({ result, token });
+    });
   } finally {
   }
 }
