034588a3a017b47934101d78a20505dadd6133e9
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-05-30 08:25:50 +0600

    Update User Information

--- index.js
+++ index.js
@@ -150,6 +150,23 @@ async function run() {
       );
       res.send({ result, token });
     });
+    // Update User Information
+    app.put("/user/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1015h" }
+      );
+      res.send({ result, token });
+    });
   } finally {
   }
 }
